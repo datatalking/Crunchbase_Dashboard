16bdd0f7c8550da5a823715569a1b8fbb3b823b6
lourdeslouis <80364775+lourdeslouis@users.noreply.github.com>
2021-07-16 21:14:36 -0700

    Update OC_Crunchbase_Queries.py

--- OC_Crunchbase_Queries.py
+++ OC_Crunchbase_Queries.py
@@ -326,7 +326,7 @@ master_clean_orgs["location_city"] = master_clean_orgs["location_identifiers"].a
 # fileNameFunding = "crunchbase_funding_rounds_" + get_date() + ".csv"
 # master_clean_funding.to_csv(fileNameFunding)
 #fileNameOrgs = "crunchbase_organizations_" + get_date() + ".csv"
-fileNameOrgs = "/datafile/crunchbase_organizations.csv"
+fileNameOrgs = "datafile/crunchbase_organizations.csv"
 master_clean_orgs.to_csv(fileNameOrgs)
 
 
