c69ddfcec8e8674a7958e2c14e7309b00c5a815c
lourdeslouis <80364775+lourdeslouis@users.noreply.github.com>
2021-07-18 00:10:22 -0700

    Update OC_Crunchbase_Queries.py

--- OC_Crunchbase_Queries.py
+++ OC_Crunchbase_Queries.py
@@ -322,7 +322,7 @@ master_clean_orgs["location_city"] = master_clean_orgs["location_identifiers"].a
 # Inspect the data
 # master_clean_orgs.info()
 # Add id as first column header
-master_clean_orgs.rename(index={0: "id"})
+master_clean_orgs.rename(index={0: "id"}, inplace=True)
 
 # Generates a name (including the date) for the file
 # fileNameFunding = "crunchbase_funding_rounds_" + get_date() + ".csv"
