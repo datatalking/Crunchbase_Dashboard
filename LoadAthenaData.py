450960ef97eee312bf278f40b893232b90a23822
lourdeslouis <80364775+lourdeslouis@users.noreply.github.com>
2021-07-18 23:25:10 -0700

    Update LoadAthenaData.py

--- LoadAthenaData.py
+++ LoadAthenaData.py
@@ -1,1 +1,8 @@
 
+import boto3
+
+ath = boto3.client('athena')
+
+ath.start_query_execution(
+    QueryString='create database mangolassi',
+    ResultConfiguration={'OutputLocation': 's3://mango-lassi-costings/queries/'})
