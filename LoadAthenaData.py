fd7b17dc644c3067f3dffc14ef591dfc303fcaa2
lourdeslouis <80364775+lourdeslouis@users.noreply.github.com>
2021-07-29 11:32:18 -0700

    Update LoadAthenaData.py

--- LoadAthenaData.py
+++ LoadAthenaData.py
@@ -1,7 +1,7 @@
 
 import boto3
 
-ath = boto3.client('athena')with open('crunchbase_organizations.ddl') as ddl:
+ath = boto3.client('athena') with open(sys.argv[1]) as ddl:
     ath.start_query_execution(
         QueryString=ddl.read(),
-        ResultConfiguration={'OutputLocation': 's3://crunchbase_dashboard/'})
+        ResultConfiguration={'OutputLocation': 's3://'+ sys.argv[2] + '/'});
