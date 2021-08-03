c4ac1e2cfb810416c521f851ce0e5a0961609e97
lourdeslouis <80364775+lourdeslouis@users.noreply.github.com>
2021-08-02 18:13:43 -0700

    Update LoadAthenaData.py

--- LoadAthenaData.py
+++ LoadAthenaData.py
@@ -1,8 +1,15 @@
 import boto3
 
-ath = boto3.client('athena', region_name='us-west-1')
+ddl_script_name = str(sys.argv[1])
+arg_aws_access_key_id = str(sys.argv[2])
+arg_aws_secret_access_key = str(sys.argv[3])
+arg_region_name = str(sys.argv[4])
+s3_bucket_name = str(sys.argv[5])
 
-with open('crunchbase_organizations.ddl') as ddl:
+ath = boto3.client('athena', region_name=arg_region_name, aws_access_key_id=arg_aws_access_key_id,
+    aws_secret_access_key=arg_aws_secret_access_key)
+
+with open(ddl_script_name) as ddl:
     ath.start_query_execution(
         QueryString=ddl.read(),
-        ResultConfiguration={'OutputLocation': 's3://crunchbase_dashboard/'})
+        ResultConfiguration={'OutputLocation': 's3://'+ s3_bucket_name + '/'})
