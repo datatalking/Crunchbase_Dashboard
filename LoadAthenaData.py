7c1bcd719ac149a0d30bceaf58ec0abc52257533
lourdeslouis <80364775+lourdeslouis@users.noreply.github.com>
2021-08-14 22:44:50 -0700

    Update LoadAthenaData.py

--- LoadAthenaData.py
+++ LoadAthenaData.py
@@ -11,7 +11,7 @@ s3_bucket_name = str(sys.argv[5])
 ath = boto3.client('athena', region_name=arg_region_name, aws_access_key_id=arg_aws_access_key_id,
     aws_secret_access_key=arg_aws_secret_access_key)
 
-response = client.start_query_execution(
+response = ath.start_query_execution(
     QueryString='DROP TABLE IF EXISTS crunchbase_dashboard.crunchbase_organizations',
     QueryExecutionContext={
         'Database': 'crunchbase_dashboard'
